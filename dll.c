1d1565150c443b36268a5d4e7b5e6fac3937d3ce
Brandon Ros <brandonros1@gmail.com>
2020-03-08 19:19:48 -0400

    fix relative address problems

--- dll.c
+++ dll.c
@@ -1,7 +1,12 @@
 #include <windows.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include "process.h"
+
+#define JMP_TO_LOG_PAYLOAD_SIZE 9
+#define LOG_AND_JMP_BACK_PAYLOAD_SIZE 36
+#define NUM_ORIGINAL_BYTES_REPLACED 9
 
 void init_console() {
   AllocConsole();
@@ -19,30 +24,90 @@ __declspec(dllexport) void dump(int size, const unsigned char *buf) {
   printf("\n\n");
 }
 
+uint8_t *build_log_and_jmp_back_payload(uint32_t dump_addr, uint32_t ret_addr) {
+  uint8_t *payload = malloc(LOG_AND_JMP_BACK_PAYLOAD_SIZE);
+  uint32_t index = 0;
+  // save stack, log, then restore stack
+  /* pusha */
+  payload[index++] = 0x60;
+  /* pushf */
+  payload[index++] = 0x9C;
+  /* push DWORD PTR [esp+0x28] */
+  payload[index++] = 0xFF,
+  payload[index++] = 0x74;
+  payload[index++] = 0x24;
+  payload[index++] = 0x28;
+  /* push DWORD PTR [esp+0x30] */
+  payload[index++] = 0xFF;
+  payload[index++] = 0x74;
+  payload[index++] = 0x24;
+  payload[index++] = 0x30;
+  /* mov eax, 0x00000000; address gets replaced dynamically later */
+  payload[index++] = 0xB8;
+  payload[index++] = dump_addr & 0xFF;
+  payload[index++] = (dump_addr >> 8) & 0xFF;
+  payload[index++] = (dump_addr >> 16) & 0xFF;
+  payload[index++] = (dump_addr >> 24) & 0xFF;
+  /* call eax */
+  payload[index++] = 0xFF;
+  payload[index++] = 0xD0;
+  /* add esp, 0x08 */
+  payload[index++] = 0x83;
+  payload[index++] = 0xC4;
+  payload[index++] = 0x08;
+  /* popf */
+  payload[index++] = 0x9D;
+  /* popd */
+  payload[index++] = 0x61;
+  // operations destroyed by jmp replacing
+  /* push ebp */
+  payload[index++] = 0x55;
+  /* mov ebp, esp */
+  payload[index++] = 0x8B;
+  payload[index++] = 0xEC;
+  /* sub esp, 0x164 */
+  payload[index++] = 0x81;
+  payload[index++] = 0xEC;
+  payload[index++] = 0x64,
+  payload[index++] = 0x01;
+  payload[index++] = 0x00;
+  payload[index++] = 0x00;
+  /* jmp 0x00000000; relative address gets replaced dynamically later */
+  payload[index++] = 0xE9;
+  payload[index++] = ret_addr & 0xFF;
+  payload[index++] = (ret_addr >> 8) & 0xFF;
+  payload[index++] = (ret_addr >> 16) & 0xFF;
+  payload[index++] = (ret_addr >> 24) & 0xFF;
+  return payload;
+}
+
+uint8_t *build_jmp_to_log_payload(uint32_t code_cave_addr) {
+  uint8_t *payload = malloc(JMP_TO_LOG_PAYLOAD_SIZE);
+  uint32_t index = 0;
+  /* jmp 0x00000000; relative address gets replaced dynamically later */
+  payload[index++] = 0xE9;
+  payload[index++] = code_cave_addr & 0xFF;
+  payload[index++] = (code_cave_addr >> 8) & 0xFF;
+  payload[index++] = (code_cave_addr >> 16) & 0xFF;
+  payload[index++] = (code_cave_addr >> 24) & 0xFF;
+  /* nop padding */
+  payload[index++] = 0x90;
+  payload[index++] = 0x90;
+  payload[index++] = 0x90;
+  payload[index++] = 0x90;
+  return payload;
+}
+
+uint32_t calculate_relative_address(uint32_t from, uint32_t to) {
+  return from - to - 0x05;
+}
+
 DWORD write_payload() {
   HANDLE hProcess;
-  LPVOID mem;
+  LPVOID code_cave;
   LPVOID dump_addr;
-  uint8_t asm_payload[] = {
-    0x60, /* pusha */
-    0x9C, /* pushf */
-    0xFF, 0x74, 0x24, 0x28, /* push DWORD PTR [esp+0x28] */
-    0xFF, 0x74, 0x24, 0x30, /* push DWORD PTR [esp+0x30] */
-    0xB8, 0x00, 0x00, 0x00, 0x00, /* mov eax, 0x000000; address gets replaced dynamically later */
-    0xFF, 0xD0, /* call eax */
-    0x83, 0xC4, 0x08, /* add esp, 0x08 */
-    0x9D, /* popf */
-    0x61, /* popd */
-    0x81, 0xEC, 0x1C, 0x01, 0x00, 0x00, /* sub esp, 0x11c */
-    0xE9, 0x00, 0x00, 0x00, 0x00 /* jmp 0x00000000; relative address gets replaced dynamically later */
-  };
-  uint8_t jmp_payload[] = {
-    0xE9, 0x00, 0x00, 0x00, 0x00, /* jmp 0x00000000; relative address gets replaced dynamically later */
-    0x90 /* nop */
-  };
-  uint32_t src = PATCH_SRC;
-  uint32_t dest = PATCH_SRC + 0x05;
-  uint32_t diff;
+  uint8_t *jmp_to_log_payload;
+  uint8_t *log_and_jmp_back_payload;
   // get handle
   hProcess = get_handle();
   if (!hProcess) {
@@ -51,8 +116,8 @@ DWORD write_payload() {
     return 1;
   }
   // allocate memory
-  mem = VirtualAllocEx(hProcess, NULL, sizeof(asm_payload), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
-  if (!mem) {
+  code_cave = VirtualAllocEx(hProcess, NULL, LOG_AND_JMP_BACK_PAYLOAD_SIZE, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+  if (!code_cave) {
     fprintf(stderr, "Unable to allocate memory.");
     getchar();
     return 1;
@@ -64,17 +129,11 @@ DWORD write_payload() {
     getchar();
     return 1;
   }
-  // write jump
-  diff = (unsigned int)mem - src - 5;
-  memcpy(jmp_payload + 1, &diff, 4);
-  WriteProcessMemory(hProcess, (LPVOID)src, jmp_payload, sizeof(jmp_payload), NULL);
-  printf("send -> dump jump written at %p\n\n", src);
-  // write payload
-  memcpy(asm_payload + 11, &dump_addr, 4);
-  diff = dest - ((unsigned int)mem + 0x1C) - 5;
-  memcpy(asm_payload + 29, &diff, 4);
-  WriteProcessMemory(hProcess, mem, asm_payload, sizeof(asm_payload), NULL);
-  printf("Payload written at %p\n\n", mem);
+  jmp_to_log_payload = build_jmp_to_log_payload(calculate_relative_address(code_cave, PATCH_SRC));
+  log_and_jmp_back_payload = build_log_and_jmp_back_payload(dump_addr, calculate_relative_address(PATCH_SRC + 0x05, code_cave + 0x1F));
+  WriteProcessMemory(hProcess, (LPVOID)PATCH_SRC, jmp_to_log_payload, JMP_TO_LOG_PAYLOAD_SIZE, NULL);
+  WriteProcessMemory(hProcess, code_cave, log_and_jmp_back_payload, LOG_AND_JMP_BACK_PAYLOAD_SIZE, NULL);
+  printf("Payload written at %p\n\n", code_cave);
   return 0;
 }
 
