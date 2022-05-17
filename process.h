519020380cd7e216ac86470e62d59852216c40fc
Brandon Ros <brandonros1@gmail.com>
2022-05-16 22:03:48 -0400

    bump for 7.52

--- process.h
+++ process.h
@@ -2,17 +2,17 @@
 #define PROCESS_H
 #include <stdint.h>
 
-// 2020/03/08, client EXE version 7.50
+// 2022/05/16, client EXE version 7.52 (with patch)
 static uint8_t original_instructions[] = {
   // push ebp
   0x55,
   // mov ebp, esp
   0x8B, 0xEC,
-  // sub esp, 0x164
-  0x81, 0xEC, 0x64, 0x01, 0x00, 0x00
+  // sub esp, 0x130
+  0x81, 0xEC, 0x30, 0x01, 0x00, 0x00
 };
 #define ORIGINAL_INSTRUCTIONS_SIZE sizeof(original_instructions)
-#define PATCH_SRC 0x005BEF90
+#define PATCH_SRC 0x00576660
 #define ARG_1_STACK_OFFSET 0x08
 #define ARG_2_STACK_OFFSET 0x0C
 #define NUM_ARGS 2
