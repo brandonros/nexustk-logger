9d97db332355e184b49c67e8e2dd189af3458d69
Brandon Ros <brandonros1@gmail.com>
2022-05-16 22:05:35 -0400

    reuse dockcross in home directory

--- compile.sh
+++ compile.sh
@@ -3,11 +3,11 @@ set -e
 # cleanup
 rm -f nexustk-logger.dll nexustk-logger.exe
 # init compiler
-if [ ! -f dockcross-windows-shared-x86 ]
+if [ ! -f ~/dockcross-windows-shared-x86 ]
 then
-  docker run --rm dockcross/windows-shared-x86 > ./dockcross-windows-shared-x86
-  chmod +x ./dockcross-windows-shared-x86
+  docker run --rm dockcross/windows-shared-x86 > ~/dockcross-windows-shared-x86
+  chmod +x ~/dockcross-windows-shared-x86
 fi
 # compile
-./dockcross-windows-shared-x86 bash -c '$CC process.c dll.c -shared -o nexustk-logger.dll -lpsapi'
-./dockcross-windows-shared-x86 bash -c '$CC process.c main.c -o nexustk-logger.exe -lpsapi'
+~/dockcross-windows-shared-x86 bash -c '$CC process.c dll.c -shared -o nexustk-logger.dll -lpsapi'
+~/dockcross-windows-shared-x86 bash -c '$CC process.c main.c -o nexustk-logger.exe -lpsapi'
