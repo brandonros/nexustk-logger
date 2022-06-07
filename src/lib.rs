c5241471969381314a67584e7b018a7cd6951776
Brandon Ros <brandonros1@gmail.com>
2022-06-07 01:07:19 -0400

    fix bugs

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,37 @@
-use winapi::um::winnt::{DLL_PROCESS_ATTACH, LPCWSTR};
+#![feature(abi_thiscall)]
 
-detour::static_detour! {
-  static BuildPacketHook: fn(*u8, u16);
+use std::ffi::c_void;
+use detour::static_detour;
+use winapi::um::winnt::{DLL_PROCESS_ATTACH};
+use winapi::shared::minwindef::{HINSTANCE, DWORD, BOOL, TRUE, LPVOID};
+
+static_detour! {
+  static BuildPacketHook: unsafe extern "thiscall" fn(*const c_void, *const u8, u16);
 }
 
-fn our_BuildPacket(input: u8, input_size: u16) {
+type FnBuildPacket = unsafe extern "thiscall" fn(*const c_void, *const u8, u16);
+
+fn our_BuildPacket(this: *const c_void, input: *const u8, input_size: u16) {
   let input_slice = unsafe { std::slice::from_raw_parts(input, input_size as usize) };
   for i in 0..input_size as usize {
     print!("{:02x}", input_slice[i]);
   }
   println!("");
-  BuildPacketHook.call(input, input_size);
+  unsafe { BuildPacketHook.disable().unwrap() };
+  unsafe { BuildPacketHook.call(this, input, input_size) };
+  unsafe { BuildPacketHook.enable().unwrap() };
 }
 
 // entry point
 #[no_mangle]
 #[allow(non_snake_case, unused_variables)]
-pub extern "system" fn DllMain(dll_module: winapi::HINSTANCE, call_reason: winapi::DWORD, reserved: winapi::LPVOID) -> winapi::BOOL {
-  if (call_reason == DLL_PROCESS_ATTACH) {
-    unsafe { kernel32::AllocConsole() };
+pub extern "system" fn DllMain(dll_module: HINSTANCE, call_reason: DWORD, reserved: LPVOID) -> BOOL {
+  if call_reason == DLL_PROCESS_ATTACH {
+    unsafe { winapi::um::consoleapi::AllocConsole() };
     println!("Initializing...");
-    let client_buildPacket: usize = 0x00576660;
+    let client_buildPacket: FnBuildPacket = unsafe { std::mem::transmute(0x00576660) };
     unsafe { BuildPacketHook.initialize(client_buildPacket, our_BuildPacket).unwrap() };
     unsafe { BuildPacketHook.enable().unwrap() };
   }
-  return winapi::TRUE;
+  return TRUE;
 }
