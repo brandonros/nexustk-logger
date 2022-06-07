c9df7b029b362bd1945e4faf0791cfd2aaa256d9
Brandon Ros <brandonros1@gmail.com>
2022-06-07 01:13:12 -0400

    outgoing

--- src/lib.rs
+++ src/lib.rs
@@ -6,20 +6,20 @@ use winapi::um::winnt::{DLL_PROCESS_ATTACH};
 use winapi::shared::minwindef::{HINSTANCE, DWORD, BOOL, TRUE, LPVOID};
 
 static_detour! {
-  static BuildPacketHook: unsafe extern "thiscall" fn(*const c_void, *const u8, u16);
+  static CryptOutgoingPacketHook: unsafe extern "thiscall" fn(*const c_void, *const u8, u16);
 }
 
-type FnBuildPacket = unsafe extern "thiscall" fn(*const c_void, *const u8, u16);
+type FnCryptOutgoingPacket = unsafe extern "thiscall" fn(*const c_void, *const u8, u16);
 
-fn our_BuildPacket(this: *const c_void, input: *const u8, input_size: u16) {
+fn our_cryptOutgoingPacket(this: *const c_void, input: *const u8, input_size: u16) {
   let input_slice = unsafe { std::slice::from_raw_parts(input, input_size as usize) };
   for i in 0..input_size as usize {
     print!("{:02x}", input_slice[i]);
   }
   println!("");
-  unsafe { BuildPacketHook.disable().unwrap() };
-  unsafe { BuildPacketHook.call(this, input, input_size) };
-  unsafe { BuildPacketHook.enable().unwrap() };
+  unsafe { CryptOutgoingPacketHook.disable().unwrap() };
+  unsafe { CryptOutgoingPacketHook.call(this, input, input_size) };
+  unsafe { CryptOutgoingPacketHook.enable().unwrap() };
 }
 
 // entry point
@@ -29,9 +29,9 @@ pub extern "system" fn DllMain(dll_module: HINSTANCE, call_reason: DWORD, reserv
   if call_reason == DLL_PROCESS_ATTACH {
     unsafe { winapi::um::consoleapi::AllocConsole() };
     println!("Initializing...");
-    let client_buildPacket: FnBuildPacket = unsafe { std::mem::transmute(0x00576660) };
-    unsafe { BuildPacketHook.initialize(client_buildPacket, our_BuildPacket).unwrap() };
-    unsafe { BuildPacketHook.enable().unwrap() };
+    let client_cryptOutgoingPacket: FnCryptOutgoingPacket = unsafe { std::mem::transmute(0x00576660) };
+    unsafe { CryptOutgoingPacketHook.initialize(client_cryptOutgoingPacket, our_cryptOutgoingPacket).unwrap() };
+    unsafe { CryptOutgoingPacketHook.enable().unwrap() };
   }
   return TRUE;
 }
